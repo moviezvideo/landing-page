ed7304310d09330ce32224c3d4ce176adc9aa020
Luiz Henrique Almeida da Silva <luizhrqas@gmail.com>
2018-02-07 04:59:20 -0200

    added icons

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import Logo from "./components/Logo";
+import adsIconSvg from "./assets/ads-icon.svg";
+import anonymousIconSvg from "./assets/anonymous-icon.svg";
+import opensourceIconSvg from "./assets/opensource-icon.svg";
 import "./stylesheet.css";
 
 class App extends Component {
@@ -19,6 +22,39 @@ class App extends Component {
                 A easy way to watch your series and movies
               </h1>
             </Col>
+          </Row>
+          <Row className="app__icons">
+            <Col xs={7}>
+              <Row center="xs" around="xs">
+                <Col xs={3}>
+                  <img
+                    src={anonymousIconSvg}
+                    className="app__icon"
+                    alt="Anonymous"
+                  />
+                  <p className="app__icon__description">
+                    100% safe<br />Without login
+                  </p>
+                </Col>
+                <Col xs={3}>
+                  <img
+                    src={opensourceIconSvg}
+                    className="app__icon"
+                    alt="Open source"
+                  />
+                  <p className="app__icon__description">
+                    Open source<br />Self-hosted<br />
+                  </p>
+                </Col>
+                <Col xs={3}>
+                  <img src={adsIconSvg} className="app__icon" alt="No ads" />
+                  <p className="app__icon__description">
+                    No ads<br />Now and never<br />
+                  </p>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={5}>form aqui</Col>
           </Row>
         </Grid>
       </div>
